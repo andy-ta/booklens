74694a3aaeb4b5e7ce1cac5854decca85b11a92f
Kevin Ding <aimixiu1993@gmail.com>
2020-01-25 14:44:49 -0500

    completed image to text with filtered results

--- server/controllers/i2tController.js
+++ server/controllers/i2tController.js
@@ -7,18 +7,23 @@ const client = new vision.ImageAnnotatorClient();
  * TODO(developer): Uncomment the following line before running the sample.
  */
 
-const fileName = 'C:/Users/Rosa/Pictures/sign_small.jpg';
+const fileName = 'assets/fatChick.jpg';
 
 exports.hello = (req, res) => {
   // Performs text detection on the local file
   client.textDetection(fileName)
     .then((result) => {
     const detections = result[0].textAnnotations;
-    console.log('Text:');
     const filter = detections.filter((value) => {
       return value.description.split(" ").length === 1;
     });
-      console.log(filter);
+    console.log(filter);
+    const filteredResults = [];
+    filter.forEach(wordObj => {
+      let filteredWordObj = (({description, boundingPoly : { vertices }}) => ({word: description, vertices}))(wordObj);
+      filteredResults.push(filteredWordObj);
+    });
+    res.json(filteredResults);
   })
     .catch((err) => {
       console.log(err);
