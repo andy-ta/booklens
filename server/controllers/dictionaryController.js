ea613e35bc81f0414055388bf1b25968c2493356
Caleb Mech <cmech1@gmail.com>
2020-01-25 18:27:08 -0500

    Dictionary now gives syllables and IPA in arrays

--- server/controllers/dictionaryController.js
+++ server/controllers/dictionaryController.js
@@ -1,25 +1,28 @@
-const axios = require('axios');
+const axios = require("axios");
 const dictAPI = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/`;
 const url = keyword => {
-  return dictAPI + keyword + `/?key=${process.env.DIC_API_KEY}`
+    return dictAPI + keyword + `/?key=${process.env.DIC_API_KEY}`;
 };
 
-exports.getSyllable = (req, res) =>  {
-  const { word } = req.body;
-  axios
-    .get(url(word))
-    .then(r => {
-      let result;
-      if (r.data.length && r.data[0].hwi) {
-        result = (({ mw }) => ({syllable : mw}))(r.data[0].hwi.prs[0]);
-      } else {
-        result = {};
-      }
-      res.json(result);
-    })
-    .catch(e => {
-      console.log(e);
-      res.statusCode = 500;
-      res.json(e);
-    });
+exports.getSyllable = (req, res) => {
+    const { word } = req.body;
+    axios
+        .get(url(word))
+        .then(r => {
+            let result;
+            if (r.data.length && r.data[0].hwi) {
+                result = (({ hw, prs }) => ({
+                    syllable: hw.split("*"),
+                    ipa: prs[0].mw.split("-")
+                }))(r.data[0].hwi);
+            } else {
+                result = {};
+            }
+            res.json(result);
+        })
+        .catch(e => {
+            console.log(e);
+            res.statusCode = 500;
+            res.json(e);
+        });
 };
