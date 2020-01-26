3e464743c28ab6273b2c9a51be522fbe0d4f50f9
Caleb Mech <cmech1@gmail.com>
2020-01-26 10:43:38 -0500

    Andy

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -17,18 +16,18 @@ app.use('/api/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send('Error');
+    // render the error page
+    res.status(err.status || 500);
+    res.send('Error');
 });
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: __dirname + '/../assets'});
+const upload = multer({ dest: __dirname + '/../assets' });
 const router = express.Router();
 require('dotenv').config();
 const dictionaryController = require('../controllers/dictionaryController');
